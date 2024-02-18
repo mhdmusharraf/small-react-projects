989174832172c40ba0bc266a9b311deab651222c
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-02-18 19:37:43 +0530

    BMI Calculator app created

--- BMI Calculator/src/App.jsx
+++ BMI Calculator/src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [bmi, setBmi] = useState(null);
+  const [bmiStatus, setBmiStatus] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const calculateBmi = () => {
+    const isValidHeight = /^\d+$/.test(height);
+    const isValidWeight = /^\d+$/.test(weight);
+    if(isValidHeight && isValidWeight){
+      const heightInMeters = height / 100;
+      const bmiValue = weight / (heightInMeters * heightInMeters);
+      setBmi(bmiValue.toFixed(2));
+      if(bmiValue<18.5){
+        setBmiStatus("Under Weight");
+      }else if(bmiValue>=18.5 && bmiValue<=24.9){
+        setBmiStatus("Normal Weight");
+      }else if(bmiValue>=25 && bmiValue<29.9){
+        setBmiStatus("Over Weight");
+      }else{
+        setBmiStatus("Obesity");
+      }
+      setErrorMessage("");
+    }else{
+      setBmi(null);
+      setBmiStatus("");
+      setErrorMessage("Please enter valid numeric values for height and weight");
+    }
+  }
 
+  const clearAll = () =>{
+    setHeight("");
+    setWeight("");
+    setBmi(null);
+    setBmiStatus("");
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bmi-calculator">
+        <div className="box"> </div>
+        <div className="data">
+          <h1>BMI Calculator</h1>
+          {errorMessage && <p className="error">{errorMessage}</p>}
+          <div className="input-container">
+            <label htmlFor="height">Height (cm):</label>
+            <input type="text" id="height" value={height} onChange={(e)=>setHeight(e.target.value)} />
+          </div>
+          <div className="input-container">
+            <label htmlFor="weight">Weight (kg):</label>
+            <input type="text" value={weight} onChange={(e)=>setWeight(e.target.value)} id="weight" />
+          </div>
+          <button onClick={calculateBmi}>Calculate BMI</button>
+          <button onClick={clearAll}>Clear</button>
+          {bmi !== null &&(<div className="result">
+            <p>Your BMI is: {bmi}</p>
+            <p>Status: {bmiStatus}</p>
+          </div>)}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
